6b0e9b3ce24a070f34a750cb0a2368990deae724
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-10-28 10:50:37 +0300

    created Recipe Module ang Routing Module

---
+++ modules_and_optimizing/modules_optimizing_apps/src/app/recipes/recipe.module.ts
@@ -0,0 +1,31 @@
+import { RecipeRoutingModule } from './recipe-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+
+@NgModule({
+  declarations: [
+    RecipeStartComponent,
+    RecipesComponent,
+    RecipeEditComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RecipeRoutingModule
+  ]
+})
+export class RecipeModule {
+
+}
