38c965f93e72959300fba345423ee3864bc687c8
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-10-28 11:09:08 +0300

    added Shared Module

--- modules_and_optimizing/modules_optimizing_apps/src/app/app.module.ts
+++ modules_and_optimizing/modules_optimizing_apps/src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
-import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+
+// Services
+import { DataStorageService } from './shared/data-storage.service';
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 
+// Modules
+import { SharedModule } from './shared/shared.module';
 import { RecipeModule } from './recipes/recipe.module';
 
 @NgModule({
@@ -27,14 +31,14 @@ import { RecipeModule } from './recipes/recipe.module';
     ShoppingEditComponent,
     SignupComponent,
     SigninComponent,
-    DropdownDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     AppRoutingModule,
-    RecipeModule
+    RecipeModule,
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
--- modules_and_optimizing/modules_optimizing_apps/src/app/recipes/recipe.module.ts
+++ modules_and_optimizing/modules_optimizing_apps/src/app/recipes/recipe.module.ts
@@ -1,14 +1,18 @@
-import { RecipeRoutingModule } from './recipe-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+// Components
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipesComponent } from './recipes.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+
+// Modules
+import { SharedModule } from './../shared/shared.module';
+import { RecipeRoutingModule } from './recipe-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,12 +22,12 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RecipeRoutingModule
+    RecipeRoutingModule,
+    SharedModule
   ]
 })
 export class RecipeModule {
